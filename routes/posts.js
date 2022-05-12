823866b539ea8ef48e09a88dcb5b7d9147afd0d2
meghaMaheshawri17 <megha.maheshwari100@gmail.com>
2022-05-12 20:05:58 +0530

    check authentication on creating posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,11 +1,13 @@
 //firing up express to get express.router to separate routes and controllers
 const express = require('express');
+const passport = require('passport');
 const router = express.Router();
 
 //accessing post controller
 const postController =require('../controllers/posts_controller');
  
-router.post('/create',postController.create);
+//passport.checkAuthentication so that unauthenticated users can not post on home 
+router.post('/create',passport.checkAuthentication,postController.create);
 
 //exporting it
 module.exports=router;
