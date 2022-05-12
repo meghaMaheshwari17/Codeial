3dc97fca1c4b6db6ffe1b5d02125e34953b52348
meghaMaheshawri17 <megha.maheshwari100@gmail.com>
2022-05-12 19:49:48 +0530

    Displaying posts and related user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,10 +1,25 @@
+const Post=require('../models/post');
+//importing post schema to show posts
+
+
 //exporting it and we need to acccess it in routes index.js
 module.exports.home=function(req,res){
     //cookie coming in as request
-    console.log(req.cookies);
+   // console.log(req.cookies);
     //changing the cookie in response         
     //res.cookie('user_id',25);
     // return res.end('<h1>returning from home controller</h1>');
-    return res.render('home',{title:"Home"}) //rendering home.ejs file
+    
+    //old post db request which didn't have user name
+    // Post.find({},function(err, posts){
+    //     if(err){console.log('Error in displaying posts'); return;}
+    //     return res.render('home',{title:"Codeial | Home",posts:posts}) //rendering home.ejs file
+    // });  
+
+    Post.find({}).populate('user').exec(function(err, posts){
+        if(err){console.log('Error in displaying posts'); return;}
+        return res.render('home',{title:"Codeial | Home",posts:posts}) //rendering home.ejs file
+    });//prepopulating posts so user will contain the whole user data from db 
+   
 }
 
