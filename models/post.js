9506b58f84158ed052d57431c30aefd51d440db5
meghaMaheshawri17 <megha.maheshwari100@gmail.com>
2022-05-13 10:43:38 +0530

    creating schema for comments

--- models/post.js
+++ models/post.js
@@ -10,6 +10,14 @@ const postSchema=new mongoose.Schema({
        type:mongoose.Schema.Types.ObjectId, //will refer to user's schema
        ref:'User' //User is the shema we are refering to
     },
+    //include the array of ids of comments so that fetching of comments on a post will be faster
+    //instead of going to each comment and finding out where postid is this post, we are already populating post with comments whose postid matches with this post   
+    comments:[
+        {
+            type:mongoose.Schema.Types.ObjectId, //will refer to comments's schema
+            ref:'Comment'   
+        }
+    ]
 },{
     timestamps:true 
 });
